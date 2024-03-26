9d7fbec113a4ab687f4d7b93ff3cadc94aa7194e
Marcelkress <Marcel@kress.dk>
2024-03-26 17:57:12 +0100

    finished

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -1,49 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-using System.Xml;
-using System.IO;
-using System.Xml.Linq;
-
-using System.Xml.Serialization;
 using System;
+using System.IO;
+using System.Xml.Serialization;
 
+
+[Serializable]
+public struct Person //Creating a struct to store the data
+{
+    public string Name;
+    public int DateOfBirth;
+    public string FavouriteColor;
+    public Person(string name, int dateOfBirth, string favouriteColor)
+    {
+        Name = name;
+        DateOfBirth = dateOfBirth;
+        FavouriteColor = favouriteColor;
+    }
+}
+
+[Serializable]
+public class ListOfPeople   //Creating a class to store the list of people for json serialization, as jsonutility does not support list serialization
+{
+    public List<Person> GroupOfPeople; 
+}
 
 public class DataManager : MonoBehaviour
 {
+    // List of people
+    public List<Person> PersonList = new List<Person>
+    {
+        new Person ("Greg", 100, "Blue"),
+        new Person ("John", 153, "Green"),
+        new Person ("Alice", 200, "Red")
+    };
+
     // XML stuff
-    private string _xmlData;    // Used for creating file
+    private string _xmlData;    // Used for creating file, this will be the file name and path
     private string _xmlDataPath;    // Used for creating directory
 
     // Json stuff
     private string _jsonDataPath; // Used for creating directory
-    private string _jsonData;   // Used for creating file
+    private string _jsonData;   // Used for creating file, this will be the file name and path
 
     // Person stuff
-    [Serializable]
-    public struct PersonData //Creating a struct to store the data
-    {
-        public string Name;
-        public string DateOfBirth;
-        public string FavouriteColor;
-    }
-
-    [Serializable]
-    public class People
-    {
-        public List<PersonData> PersonList { get; set; }
-    }
 
     void Awake()
     {
         _xmlDataPath = Application.persistentDataPath + "/XMLDataToSave/";  // Path to save XML data folder
         _jsonDataPath = Application.persistentDataPath + "/JSONDataToSave/";// Path to save JSON data folder
 
-        _jsonData = _jsonDataPath + "Person_Data.json"; //Path to save JSON file
-        _xmlData = _xmlDataPath + "Person_Data.xml";    //Path to save XML file
+        _jsonData = _jsonDataPath + "Person_Data.json"; //Path to save JSON file, and the file name
+        _xmlData = _xmlDataPath + "Person_Data.xml";    //Path to save XML file, and the file name
 
-        Debug.Log("XML Path: " + _xmlDataPath);
+        Debug.Log(Application.persistentDataPath);      // Path to the data path unity uses to save data
     }
 
     void Start()
@@ -53,45 +64,18 @@ public class DataManager : MonoBehaviour
 
     private void Initialize()
     {
-        NewDirectory(_xmlDataPath);
-        NewDirectory(_jsonDataPath);
+        NewDirectory(_xmlDataPath); //Create a new directory(folder) for XML data
+        NewDirectory(_jsonDataPath);//Create a new directory(folder) for JSON data
 
-        InitData();
+        //SerializeXML();
+        //DeserializeXML();
 
-        //WriteToXML(_xmlData, InitData());
-
-        People peopleFromXML = ReadXML(_xmlData);
-
-        //WriteToJSON(_jsonData, peopleFromXML.PersonList);
+        SerializeJSON();
     }
 
-    public People InitData()
-    {
-        People people = new People
-        {
-            PersonList = new List<PersonData>
-            {
-                new PersonData
-                {
-                    Name = "Jeff",
-                    DateOfBirth = "1995-01-01",
-                    FavouriteColor = "Blue"
-                },
-                new PersonData
-                {
-                    Name = "Alice",
-                    DateOfBirth = "1996-02-02",
-                    FavouriteColor = "Red"
-                }
-            }
-        };
-        return people;
-    }
 
-    /// <summary>
-    /// Creates a new directory if it does not exist
-    /// </summary>
-    /// <param name="dataPath"></param>
+
+    // Creates a new directory if it does not exist
     public void NewDirectory(string dataPath)
     {
         if(Directory.Exists(dataPath))
@@ -104,90 +88,63 @@ public class DataManager : MonoBehaviour
         Debug.Log("Directory created");
     }
 
-    /// <summary>
-    /// Writes to XML file
-    /// </summary>
-    /// <param name="filename"></param>
-    public void WriteToXML(string filename, People people)
+    public void SerializeXML()
     {
-        // Check if file already exists
-        if(File.Exists(filename))
+        if(File.Exists(_xmlData))   //Check if file already exists, if it does, do not create a new file - we can remove this to overwrite the file
         {
             Debug.Log("File already exists");
             return;
         }
 
-        var xmlSerializer = new XmlSerializer(typeof(People));
+        var xmlSerializer = new XmlSerializer(typeof(List<Person>));
 
-        using (FileStream stream = File.Create(filename))
+        using(FileStream stream = File.Create(_xmlData))
         {
-            xmlSerializer.Serialize(stream, people);
+            xmlSerializer.Serialize(stream, PersonList);
         }
-    
-        Debug.Log("XML File created");
+
+        Debug.Log("XML file created");
     }
 
-    public People ReadXML(string filename)
+    public List<Person> DeserializeXML()
     {
-        // Check if file exists
-        if(!File.Exists(filename))
+        if(File.Exists(_xmlData))
         {
-            Debug.Log("File does not exist");
-            return null;
-        }
-
-        var xmlSerializer = new XmlSerializer(typeof(People));
-
-        using (FileStream stream = File.OpenRead(filename))
-        {       
-            return (People)xmlSerializer.Deserialize(stream); 
-        }
-
-    }
-    
-    public void WriteToJSON(string filename, List<PersonData> people)
-    {
-         if(people == null)
-        {
-            Debug.Log("No data to convert");
-            return;
-        }
-
-        var xmlSerializer = new XmlSerializer(typeof(List<PersonData>));
-
-        using (FileStream stream = File.OpenRead(filename))
-        {
-            var personDatas = (List<PersonData>)xmlSerializer.
-
-            Deserialize(stream);
- 
-           foreach (var peoples in personDatas)
-            {
-               Debug.LogFormat("Name: {0}, Age: {1}, Color: {3}", peoples.Name, peoples.Name, peoples.FavouriteColor);
-            }
-        }
-    }
-    /*
-    public void DeserializeXML()
-    {
-    if (File.Exists(_xmlData))
-        {
-            var xmlSerializer = new XmlSerializer(typeof(List<People>));
+            var xmlSerializer = new XmlSerializer(typeof(List<Person>));
 
             using (FileStream stream = File.OpenRead(_xmlData))
             {
-                var persons = (List<People>)xmlSerializer.
-                Deserialize(stream);
+                var people = (List<Person>)xmlSerializer.Deserialize(stream);
 
-                foreach (var person in persons)
+                foreach (var person in people)
                 {
-                    //Debug.LogFormat("Name: {0}, Age: {1}, Color: {3}", weapon.Name, weapon.DateOfBirth, weapon.FavouriteColor);
-                }
+                    Debug.LogFormat("Name: {0}, Date of Birth: {1}, Favourite Color: {2}", person.Name, person.DateOfBirth, person.FavouriteColor);
+                }                   
+                return people;
             }
+        }    
+        else
+            Debug.Log("File does not exist");
+            return null;
+        
+    }
 
+    public void SerializeJSON()
+    {
+        ListOfPeople listOfPeople = new ListOfPeople();
+
+        listOfPeople.GroupOfPeople = DeserializeXML();
+
+        string jsonString = JsonUtility.ToJson(listOfPeople, true);
+
+        using(StreamWriter stream = File.CreateText(_jsonData))
+        {
+            stream.WriteLine(jsonString);
         }
-    } 
-    */
+
+        Debug.Log("JSON file created");
+    }
+
 }
 
 
