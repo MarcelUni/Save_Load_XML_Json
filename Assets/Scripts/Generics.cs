da427a51636c464d3770d5341d21c04f55f29e9b
Marcelkress <Marcel@kress.dk>
2024-06-04 13:29:20 +0200

    scripts

--- Assets/Scripts/Generics.cs
+++ Assets/Scripts/Generics.cs
@@ -7,12 +7,38 @@ public class Generics : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Shop<string> itemShop = new Shop<string>();
+
+        Shop<float> floatShop = new Shop<float>();
+
+        floatShop.AddItem(5.5f);
+        Debug.Log("Items for sale: " + itemShop.inventory.Count);
     }
 
     // Update is called once per frame
     void Update()
     {
+        float five = 5;
+        GenericMethod(five);
         
+        string blob = "Blob";
+        GenericMethod(blob);
+    }
+    
+    public void GenericMethod<T>(T item)
+    {
+        Debug.Log(item);
+    }
+
+
+}
+
+public class Shop<T>
+{
+    public List<T> inventory = new List<T>();
+
+    public void AddItem(T item)
+    {
+        inventory.Add(item);
     }
 }
